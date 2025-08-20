23db09dbf3cef251d4f8d61404eccc81337f2ceb
raaddalwai786 <2025.raadd@isu.ac.in>
2025-08-21 03:48:11 +0530

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwind from '@tailwindcss/vite'
 
-
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [react(), tailwind()],
-
-  // Use '/' in dev; '/static/' in build so Django can serve built assets correctly
-  base: command === 'build' ? '/static/' : '/',
-
+  base: '/',                 // default for Vercel
   build: {
-  outDir: 'dist',     // default
-  emptyOutDir: true,
-},
-}))
+    outDir: 'dist',
+    emptyOutDir: true
+  }
+})
