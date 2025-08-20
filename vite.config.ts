3f93365bd77cd53e2f1f4a7355676edbf07b757a
raaddalwai786 <2025.raadd@isu.ac.in>
2025-08-21 03:39:45 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwind from '@tailwindcss/vite'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
 
-// ESM-safe __dirname
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
 
 export default defineConfig(({ command }) => ({
   plugins: [react(), tailwind()],
